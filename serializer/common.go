898189b1e704075646c2b8bd965685e80878fb2f
jjdtk <1403179190@qq.com>
2019-12-18 21:06:29 +0800

    增加列表构建器

--- serializer/common.go
+++ serializer/common.go
@@ -10,6 +10,22 @@ type Response struct {
 	Error string      `json:"error,omitempty"`
 }
 
+// DataList 基础列表结构
+type DataList struct {
+	Items interface{} `json:"items"`
+	Total uint        `json:"total"`
+}
+
+// BuildListResponse 列表构建器
+func BuildListResponse(items interface{}, total uint) Response {
+	return Response{
+		Data: DataList{
+			Items: items,
+			Total: total,
+		},
+	}
+}
+
 // TrackedErrorResponse 有追踪信息的错误响应
 type TrackedErrorResponse struct {
 	Response
